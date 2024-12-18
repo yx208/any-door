bbf6230e99a4291d2dbdb13cc45136d67d923a6e
yx208 <yx208.lin@gmail.com>
2024-12-18 11:25:45 +0800

    feat: client bin

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use std::sync::Arc;
+use tokio::net::TcpStream;
+use proxy_rs::config::ClientConfig;
+use proxy_rs::error::{Result, ProxyError};
+use proxy_rs::transport::stream::StreamManager;
+
+async fn handle_client_connection(
+    mut local_stream: TcpStream,
+    config: Arc<ClientConfig>,
+    stream_manager: Arc<StreamManager>
+) -> Result<()> {
+
+
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+
+    Ok(())
+}
